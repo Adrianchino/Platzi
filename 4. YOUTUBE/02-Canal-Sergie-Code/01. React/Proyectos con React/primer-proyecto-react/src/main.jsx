b46812198c3ace6f1bebc899c5c0fc8c8454c201
Adrian Rugel Mija <adrianrugel34@gmail.com>
2024-04-17 09:34:46 -0500

    Subir el primer proyecto

---
+++ 4. YOUTUBE/02-Canal-Sergie-Code/01. React/Proyectos con React/primer-proyecto-react/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './Css/style.css'
+import ContadorApp from './Componentes/ContadorApp'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ContadorApp />
+  </React.StrictMode>,
+)
